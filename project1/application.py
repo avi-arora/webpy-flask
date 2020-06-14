61a528852f8901dce8a68e933d61d5deac1ee272
avishek arora <avishek.arora@globallogic.com>
2020-06-15 03:38:54 +0530

    added login/logout

--- project1/application.py
+++ project1/application.py
@@ -1,6 +1,6 @@
 import os
-
-from flask import Flask, session, render_template
+from functools import wraps
+from flask import Flask, session, render_template, request, url_for, redirect
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -21,6 +21,16 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
+def authorize(f):
+    @wraps(f)
+    def wrapper():
+        if session.get("username") != None:
+            return f()
+        else:
+            return redirect(url_for("login"))
+    return wrapper
+
+
 @app.route("/")
 def index():
     return "Project 1: TODO"
@@ -34,10 +44,50 @@ def login():
 def register():
     return render_template("register.html")
 
+@app.route("/registeruser", methods=["POST"])
+def registeruser():
+    #extract the fields
+    pwd, confPwd = request.form.get("pwd"), request.form.get("confPwd")
+    if pwd != confPwd:
+        pass
+    else:
+        uname, email = request.form.get("uname"), request.form.get("email")
+
+        db.execute("INSERT into users(username,email,password) VALUES (:uname,:email,:pwd)",
+        {"uname":uname, "email": email, "pwd": pwd})
+        try: 
+            db.commit()
+            responseObj = {"message":"Registered successfully.", "status":"SUCCESS"}
+        except Exception:
+            responseObj = {"message":"Error,Please try again later.", "status":"ERROR"}
+            
+        return redirect(url_for("login"))
+
+@app.route("/authenticate", methods=["POST"])
+def authenticate():
+    uname, pwd = request.form.get("uname"), request.form.get("pwd")
+    user = db.execute("SELECT * FROM users WHERE email=:uname AND password=:pwd",{"uname": uname, "pwd": pwd}).fetchone()
+
+    if user: 
+        session["username"] = user.username
+    
+    return redirect(url_for("search"))
+
+
 @app.route("/search")
+@authorize
 def search():
     return render_template("search.html")
 
 @app.route("/book")
+@authorize
 def book():
     return render_template("book.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("username")
+    return redirect(url_for("login"))
+
+
+
